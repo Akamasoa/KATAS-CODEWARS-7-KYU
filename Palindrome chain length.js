0da3787ce20ca041cb07e53a17bf611b4ab8c44b
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-24 00:28:57 -0300

    Update and rename Palindrome chain length to Palindrome chain length.js

--- Palindrome chain length.js
+++ Palindrome chain length.js
@@ -1,3 +1,4 @@
+/+
 Description:
 
 Number is a palindrome if it is equal to the number with digits in reversed order. For example, 5, 44, 171, 4884 are palindromes and 43, 194, 4773 are not palindromes.
@@ -9,10 +10,11 @@ If the input number is already a palindrome, the number of steps is 0.
 Input will always be a positive integer.
 
 For example, start with 87:
++/
 
 87 + 78 = 165; 165 + 561 = 726; 726 + 627 = 1353; 1353 + 3531 = 4884
 
-4884 is a palindrome and we needed 4 steps to obtain it, so palindrome_chain_length(87) == 4
+//4884 is a palindrome and we needed 4 steps to obtain it, so palindrome_chain_length(87) == 4
 
 
 SOLUTION A)
