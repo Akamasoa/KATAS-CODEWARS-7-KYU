f89360ddc46b2d8921167d92289229bb77f54da3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-03 23:33:30 -0300

    Update 80's Kids #2: Help ALF Find His Spaceship.js

--- 80's Kids #2: Help ALF Find His Spaceship.js
+++ 80's Kids #2: Help ALF Find His Spaceship.js
@@ -22,3 +22,12 @@ If you cannot find the spaceship, the result should be
 Can you help ALF?
 */
 
+function findSpaceship(map) {
+   map = map.split("\n").reverse();
+   for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === "X") return [x, y];
+    }
+  }
+  return "Spaceship lost forever.";
+}
