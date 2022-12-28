3b16ea2de26a150cb12682cb44e655b77a47467a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-12-28 19:41:35 -0300

    Update Fizz Buzz.js

--- Fizz Buzz.js
+++ Fizz Buzz.js
@@ -14,3 +14,12 @@ Method calling example:
 fizzbuzz(3) -->  [1, 2, "Fizz"]
 */
 
+var fizzify = (fizzbuzz = function(n) {
+  return Array.apply(null, new Array(n)).map(function(e, i) {
+    return (++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i;
+  });
+});
+
+/*
+
+*/
