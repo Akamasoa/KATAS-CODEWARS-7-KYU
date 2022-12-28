8414b1075e709fb7f5cdef0f2d4fae9bb7b306a5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-12-28 19:41:58 -0300

    Update Fizz Buzz.js

--- Fizz Buzz.js
+++ Fizz Buzz.js
@@ -21,5 +21,22 @@ var fizzify = (fizzbuzz = function(n) {
 });
 
 /*
+function fizzbuzz(n)
+{
+  var i = 1, arr = [];
+  while(i <= n) {
+    var fizz = (i % 3 == 0);
+    var buzz = (i % 5 == 0);
+    if(fizz || buzz) {
+      arr.push((fizz?"Fizz":"") + (buzz?"Buzz":""));
+    }
+    else {
+      arr.push(i);
+    }
+    i++;
+  }
+  return arr;
+}
 
+var fizzify = fizzbuzz;
 */
