5e4754394544ae0cfa65f56742d35a258c45a381
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-05 22:48:42 -0300

    Update Fun with lists_ indexOf.js

--- Fun with lists_ indexOf.js
+++ Fun with lists_ indexOf.js
@@ -26,3 +26,14 @@ Fun with lists: map
 Fun with lists: reduce
 */
 
+function indexOf(head, value) {
+  let count = 0;
+  while (head) {
+    if (head.data === value) {
+      return count;
+    }
+    head = head.next;
+    count++;
+  }
+  return -1;
+}
