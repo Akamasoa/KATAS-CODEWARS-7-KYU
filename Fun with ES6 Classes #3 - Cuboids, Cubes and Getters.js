03759f86a21bb049c24e38a9c9060db33f9b905f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-05 22:45:12 -0300

    Update Fun with ES6 Classes #3 - Cuboids, Cubes and Getters.js

--- Fun with ES6 Classes #3 - Cuboids, Cubes and Getters.js
+++ Fun with ES6 Classes #3 - Cuboids, Cubes and Getters.js
@@ -21,3 +21,27 @@ Stack Overflow - What are getters and setters in ES6?
 getter - Javascript | MDN
 */
 
+class Cuboid {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  get volume() {
+    return this.length * this.width * this.height;
+  }
+  get surfaceArea() {
+    return (
+      2 *
+      (this.length * this.width +
+        this.width * this.height +
+        this.height * this.length)
+    );
+  }
+}
+
+class Cube extends Cuboid {
+  constructor(length) {
+    super(length, length, length);
+  }
+}
