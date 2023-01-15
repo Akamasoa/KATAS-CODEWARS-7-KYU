8fdc01eb514161d2b103c1e3a5942e3761799443
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-15 20:38:27 -0300

    Create Get the lost toString back.js

---
+++ Get the lost toString back.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+There are some functions whose toString methods have been modified.
+
+function foo() {}
+foo.toString = function() {
+  return 'blablabla';
+};
+Write a function _originalToString which takes a function as the argument, and returns a string which is as same as the argument's original toString's.
+
+Note: You should not change the argument.
+
+For example, the following statement should return function foo() {} instead of blablabla:
+
+_originalToString(foo); // function foo() {}
+*/
+
