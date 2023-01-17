888155fbaa5c09dd72b59b3069152db3dea6b6d1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-16 22:18:53 -0300

    Update Going to the cinema.js

--- Going to the cinema.js
+++ Going to the cinema.js
@@ -22,3 +22,12 @@ movie(100, 10, 0.95) should return 24
     (with card the total price is 235, with tickets 240)
 */
 
+function movie(card, ticket, perc) {
+  let costA = (n = 0),
+    costB = card;
+  while (Math.ceil(costB) >= costA) {
+    costA += ticket;
+    costB += ticket * Math.pow(perc, ++n);
+  }
+  return n;
+}
