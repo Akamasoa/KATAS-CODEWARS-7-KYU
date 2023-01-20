e17da10f54498d90ad901d08a28c182ccda7b607
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-19 22:10:26 -0300

    Update Greet Me.js

--- Greet Me.js
+++ Greet Me.js
@@ -12,3 +12,8 @@ var greet = function(name) {
  return `Hello ${name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase()}!`;
 };
 
+/*
+var greet = function(name) {
+  return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+};
+*/
