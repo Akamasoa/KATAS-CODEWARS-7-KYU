244d20d02e69b318958d877a625e024a3563f4a7
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-01-27 23:19:28 -0300

    Update Help Suzuki complete his chores!.js

--- Help Suzuki complete his chores!.js
+++ Help Suzuki complete his chores!.js
@@ -17,3 +17,11 @@ Chore 2 = 9hrs
 
 return [7, 8, 8, 10, 10, 11]
 */
+
+
+function choreAssignment(chores) {
+ return chores.sort((a,b) => a-b)
+  .map((cur, idx) => cur + chores[chores.length-idx-1])
+  .splice(chores.length/2, chores.length/2)
+  .sort((a,b) => a-b);
+}
