9dea11c5896f65f819988621b12ed278a5e5a04d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-02 17:16:45 -0300

    Create Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,9 @@
+/*
+Description:
+The sort function is supposed to set the global variable to 1 when it has been executed at least once but it does not.
+
+Can you figure out why?
+
+You should not modify the behavior of the sort function.
+*/
+
