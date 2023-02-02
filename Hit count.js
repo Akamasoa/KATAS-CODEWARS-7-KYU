16297010696d25d95a3ce35f04139092174aadf2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-02 17:14:43 -0300

    Update Hit count.js

--- Hit count.js
+++ Hit count.js
@@ -24,3 +24,19 @@ counterEffect("0050") // [[0],[0],[0,1,2,3,4,5],[0]]
 counterEffect("0000") // [[0],[0],[0],[0]]
 */
 
+function counterEffect(hitCount) {
+let arr0=hitCount.split('')
+let arr1=[];
+let arr2=[];
+let arr3=[];
+let arr4=[];
+for(let i=0;i<=arr0[0];i++)
+{arr1.push(i)}
+for(let i=0;i<=arr0[1];i++)
+{arr2.push(i)}
+for(let i=0;i<=arr0[2];i++)
+{arr3.push(i)}
+for(let i=0;i<=arr0[3];i++)
+{arr4.push(i)}
+return [arr1,arr2,arr3,arr4];
+}
