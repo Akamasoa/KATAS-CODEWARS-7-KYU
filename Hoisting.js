581f66561a4b4d277083d0f43086466f3e60bc8f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-03 20:34:15 -0300

    Update Hoisting.js

--- Hoisting.js
+++ Hoisting.js
@@ -14,7 +14,7 @@ function sort(array) {
   sorted = 1;
   
   // Transform the input data
-  for(var i = 0; i < array.length; i++) {
+  for(let i = 0; i < array.length; i++) {
     array[i] = 3 * array[i] + 2;
   }
   
@@ -23,10 +23,10 @@ function sort(array) {
   array = array.slice(0, mid).concat(array.slice(mid+1));
   
   // Sort the array
-  var sort = false;
-  for(var i = array.length - 1; i >= 0 && ! sort; i--) {
+  let sort = false;
+  for(let i = array.length - 1; i >= 0 && ! sort; i--) {
     sort = true;
-    for(var j = 0; j < i; j++) {
+    for(let j = 0; j < i; j++) {
       if ( array[j] > array[j+1] ) {
         swap(array, j, j+1);
         sort = false;
