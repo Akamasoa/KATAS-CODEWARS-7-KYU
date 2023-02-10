0a7800de90521ef05e4d1d3698c8ef9bed1e7acc
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-09 23:22:23 -0300

    Update How many are smaller than me?.js

--- How many are smaller than me?.js
+++ How many are smaller than me?.js
@@ -12,3 +12,8 @@ smaller([1, 2, 0]) === [1, 1, 0]
 If you've completed this one and you feel like testing your performance tuning of this same kata, head over to the much tougher version How many are smaller than me II?
 */
 
+function smaller(nums) {
+ return nums.map((n, i) => {
+   return nums.slice(i).filter(v => v < n).length;
+ });
+}
