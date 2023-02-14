b3beca8c7e8287799c11e6124f6b4d78928e48e2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-13 21:46:56 -0300

    Update How many twos?.js

--- How many twos?.js
+++ How many twos?.js
@@ -11,3 +11,11 @@ should return 7, since the factorization of 17280 is 2^7 x 5 x 3^3
 The number passed to two_count (twoCount) will always be a positive integer greater than or equal to 1.
 */
 
+function twoCount(n) {
+  let twos = 0;
+  while (n % 2 === 0) {
+    twos++;
+    n /= 2;
+  }
+  return twos;
+}
