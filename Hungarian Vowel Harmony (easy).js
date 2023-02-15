74c02b210c92bfbe956d6953ceee3176753b8153
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-14 21:07:29 -0300

    Update Hungarian Vowel Harmony (easy).js

--- Hungarian Vowel Harmony (easy).js
+++ Hungarian Vowel Harmony (easy).js
@@ -20,3 +20,11 @@ All strings are unicode strings.
 There are no grammatical exceptions in the tests.
 */
 
+function dative(word) {
+  const obj1 = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"];
+  const obj2 = ["a", "á", "o", "ó", "u", "ú"];
+  const filt = word.split("").filter(v => obj1.includes(v) || obj2.includes(v));
+  const str = filt[filt.length - 1];
+  return obj1.includes(str) ? word + "nek" : word + "nak";
+}
+
