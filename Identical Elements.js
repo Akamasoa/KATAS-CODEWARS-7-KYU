c17ecb514cd9c86e333fb73d2aff24450f59b867
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-16 22:51:26 -0300

    Update Identical Elements.js

--- Identical Elements.js
+++ Identical Elements.js
@@ -5,3 +5,6 @@ Given two arrays of integers m and n, test if they contain at least one identica
 Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
 */
 
+function duplicateElements(m, n) {
+    return m.some((v,i,arr)=>arr.includes(n[i]));
+}
