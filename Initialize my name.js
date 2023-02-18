236461b6a2dbf505b4fc43545f8f55f354f20498
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-17 22:08:02 -0300

    Update Initialize my name.js

--- Initialize my name.js
+++ Initialize my name.js
@@ -11,3 +11,9 @@ Examples
 'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
 */
 
+function initializeNames(name){
+   let arr = name.split(' ');
+   for (let i = 1; i < arr.length - 1; i++) 
+   		arr[i] = arr[i].charAt(0) + '.';
+   return arr.join(' ');
+}
