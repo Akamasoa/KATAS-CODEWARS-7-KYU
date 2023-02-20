966bace72aa8ab24e406bbb5e4ba65c9bd4a90ab
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-19 21:38:24 -0300

    Update Insert dashes.js

--- Insert dashes.js
+++ Insert dashes.js
@@ -5,3 +5,12 @@ Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that w
 Note that the number will always be non-negative (>= 0).
 */
 
+function insertDash(num) {
+ num = num.toString().split('')
+  for(let i = 0; i < num.length; i++){
+    if (num[i] % 2 > 0 && num[i + 1] % 2 > 0){
+      num[i] = num[i] + '-';
+    }
+  }
+  return num.join('')
+}
