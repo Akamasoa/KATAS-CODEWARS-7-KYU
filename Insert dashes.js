ce5b0356e43b7b76a70925a91e1fc631d564dc9e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-19 21:48:07 -0300

    Update Insert dashes.js

--- Insert dashes.js
+++ Insert dashes.js
@@ -6,11 +6,17 @@ Note that the number will always be non-negative (>= 0).
 */
 
 function insertDash(num) {
- num = num.toString().split('')
+  num = num.toString().split('');
   for(let i = 0; i < num.length; i++){
-    if (num[i] % 2 > 0 && num[i + 1] % 2 > 0){
-      num[i] = num[i] + '-';
+    if(num[i] % 2 > 0 && num[i + 1] % 2 > 0){
+      num[i] += '-';
     }
   }
-  return num.join('')
+  return num.join('');
 }
+
+/*
+ function insertDash(num) {
+   return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+}
+*/
