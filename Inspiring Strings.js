e5975f2b375fe9409b545b4a5f281a5a72844bc1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-21 20:18:39 -0300

    Update Inspiring Strings.js

--- Inspiring Strings.js
+++ Inspiring Strings.js
@@ -10,5 +10,5 @@ Example:
 */
 
 function longestWord(str){
-return str.split(' ').sort((a,b)=>a.length-b.length).slice(-1).toString();
+ return str.split(' ').sort((a,b)=>a.length-b.length).slice(-1).toString();
 }
