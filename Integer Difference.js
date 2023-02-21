f28c7d612c22e0c4fbde48419ac05c9c9b6e2b12
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-21 20:25:14 -0300

    Update Integer Difference.js

--- Integer Difference.js
+++ Integer Difference.js
@@ -10,3 +10,14 @@ For example:
 [1, 1, 3, 3], n=2             -->  4  # (1,3), (1,3), (1,3), (1,3)
 */
 
+const intDiff = (arr, n) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[j] - arr[i]) === n) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
