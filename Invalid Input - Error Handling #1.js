cf162d19271d5362f627cc8016abee8f73577a1f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-02-23 22:18:48 -0300

    Update Invalid Input - Error Handling #1.js

--- Invalid Input - Error Handling #1.js
+++ Invalid Input - Error Handling #1.js
@@ -36,3 +36,10 @@ public class Counter {
 }
 */
 
+function getCount(words) {
+  let f = typeof words === 'string';
+  return {
+    vowels: f ? words.replace(/[^aeiou]/gi,'').length : 0,
+    consonants: f ? words.replace(/[^bcdfghjklmnpqrstvwxyz]/gi,'').length : 0
+  }
+}
