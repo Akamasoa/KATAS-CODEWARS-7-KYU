524d6d339fed018d7fdb550f5e6d9cd3950b45c3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-01 18:54:10 -0300

    Update Isograms.js

--- Isograms.js
+++ Isograms.js
@@ -15,3 +15,14 @@ function isIsogram(str){
   return new Set(str.toUpperCase()).size == str.length;
 }
 
+/*
+function isIsogram(str){
+  var i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i)
+    for(j = i + 1; j < str.length; ++j)
+      if(str[i] === str[j])
+        return false;
+  return true;
+}
+*/
