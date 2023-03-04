74af057fd0bd3413d6536d8d9e521f74b11af29a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-03 22:33:00 -0300

    Update JavaScript class-like objects.js

--- JavaScript class-like objects.js
+++ JavaScript class-like objects.js
@@ -11,3 +11,12 @@ dog.name; // should == 'Max'
 dog.name = 'Lassie'; // should set name to 'Lassie'
 */
 
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
