7a06812fe1fd194064009950d08d2d18257aa5c5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-03 22:30:30 -0300

    Update JavaScript Array Filter.js

--- JavaScript Array Filter.js
+++ JavaScript Array Filter.js
@@ -7,3 +7,8 @@ The solution would work like the following:
 getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 */
 
+function getEvenNumbers(numbersArray) {
+  return numbersArray.filter(value => {
+  return value % 2 == 0;
+  });
+}
