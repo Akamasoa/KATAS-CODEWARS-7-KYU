32c066bef12822c0cace68195f86c2d654b0d463
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-08 22:05:29 -0300

    Update Jumping Number (Special Numbers Series #4).js

--- Jumping Number (Special Numbers Series #4).js
+++ Jumping Number (Special Numbers Series #4).js
@@ -58,3 +58,10 @@ function jumpingNumber(n){
   return 'Jumping!!'
 }
 
+/*
+function jumpingNumber(n) {
+  return [...(n + "")].every((c, i, a) => !i || Math.abs(c - a[i - 1]) === 1)
+    ? "Jumping!!"
+    : "Not!!";
+}
+*/
