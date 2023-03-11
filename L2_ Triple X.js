b0a42665c16e9e17ce3a56822360a30036360adc
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-11 20:35:10 -0300

    Update L2_ Triple X.js

--- L2_ Triple X.js
+++ L2_ Triple X.js
@@ -12,3 +12,20 @@ capital X's do not count as an occurrence of "x".
 if there are no "x"'s then return false
 */
 
+function tripleX(str) {
+  return /^[^x]*xxx/.test(str);
+}
+
+/*
+function tripleX(str){
+  var arr = str.split("");
+  var position = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "x") {
+      position = i;
+      break;
+    }
+  }
+  return arr[i+1] === "x" && arr[i+2] === "x" ? true : false;
+}
+*/
