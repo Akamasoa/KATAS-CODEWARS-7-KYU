b48b99b6b971df9f2bd889f7b9d93058cd15eb5e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-12 20:32:38 -0300

    Update Largest 5 digit number in a series.js

--- Largest 5 digit number in a series.js
+++ Largest 5 digit number in a series.js
@@ -15,3 +15,15 @@ Complete the solution so that it returns the greatest sequence of five consecuti
 Adapted from ProjectEuler.net
 */
 
+function solution(digits){
+  let answer = 0;
+  for (let i = 0; i < digits.length; i++) {
+    let number = digits.substr(i, 5); //each loop iteration pulls the next 5 digits into a substring
+    if (Number(number) > answer) {
+      //convert to number and compare against answer
+      answer = Number(number);
+    }
+  }
+  return answer;
+}
+
