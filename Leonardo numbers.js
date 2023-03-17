10a7472558fb92d6aa4d5136979c9b4c23a3418d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-17 19:50:38 -0300

    Update Leonardo numbers.js

--- Leonardo numbers.js
+++ Leonardo numbers.js
@@ -35,3 +35,10 @@ Note
 n will always be greater than or equal to 2
 */
 
+L = (n , L0 , L1 , add) => {
+  let newArr = [L0, L1];
+  for (let i = 2; i < n; i++) {
+    newArr.push(newArr[i - 2] + newArr[i - 1] + add);
+  }
+  return newArr;
+}
