99eccffd97dc0f53d2e386d1541a94961d1831b1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-17 19:53:36 -0300

    Update Letterbox Paint-Squad.js

--- Letterbox Paint-Squad.js
+++ Letterbox Paint-Squad.js
@@ -39,3 +39,12 @@ Notes
 In C, the returned value will be free'd.
 */
 
+const paintLetterboxes = (start, end) => {
+  let res = Array(10).fill(0);
+  for (let i = start; i <= end; i++) {
+  	for (let n of (i + '')) {
+    	res[n]++;
+    }
+  }
+  return res;
+}
