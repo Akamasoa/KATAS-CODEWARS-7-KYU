507f2d2142173252a8cbc7805d911b0dac500384
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-21 18:54:45 -0300

    Create Longest vowel chain.js

---
+++ Longest vowel chain.js
@@ -0,0 +1,7 @@
+/*
+Description:
+The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only and no spaces, return the length of the longest vowel substring.
+
+Good luck!
+*/
+
