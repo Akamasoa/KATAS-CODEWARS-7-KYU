cb26ef2b5203103743888fab685fd429584baca3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-23 21:50:36 -0300

    Update Love vs friendship.js

--- Love vs friendship.js
+++ Love vs friendship.js
@@ -13,3 +13,12 @@ Your task is to write a function which calculates the value of a word based off 
 The input will always be made of only lowercase letters and will never be empty.
 */
 
+function wordsToMarks(string){
+  const alphabet = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8, i:9, j:10, k:11, l:12, m:13, n:14, o:15, p:16, q:17, r:18, s:19, t:20, u:21, v:22, w:23, x:24, y:25, z:26};
+  let response = 0;
+  for (let i =0; i < string.length; i++){
+    response += alphabet[string[i]];  
+  }
+  return response
+}
+
