480322bf2ecec59c47d9e250bd6588e6fdf8ba19
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-24 23:46:06 -0300

    Update Lucky Bus Ticket.js

--- Lucky Bus Ticket.js
+++ Lucky Bus Ticket.js
@@ -10,3 +10,13 @@ isLucky('22') => false // :(
 isLucky('abcdef') => false // :(
 */
 
+function isLucky(ticket) {
+  if (ticket.length !== 6) {
+    return false;
+  }
+  if ((Number(ticket[0]) + Number(ticket[1]) + Number(ticket[2])) === (Number(ticket[3]) + Number(ticket[4]) + Number(ticket[5]))) {
+    return true;
+  } else {
+    return false;
+  }
+}
