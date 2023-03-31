b865ee39653039562b7f4801c3a36dcf3fff22a3
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-03-31 08:50:38 -0300

    Update Matrix creation.js

--- Matrix creation.js
+++ Matrix creation.js
@@ -8,11 +8,16 @@ Some examples:
 
 (2) => [ [1,0],
          [0,1] ]
+
        [ [1,0,0,0,0],
          [0,1,0,0,0],
-
 (5) =>   [0,0,1,0,0],
          [0,0,0,1,0],
          [0,0,0,0,1] ]   
 */
 
+function getMatrix(number) {
+   let arr=[];
+   for (let i=0;i<number;i++) arr.push(Array.from({length:number},(v,j)=>j===i?1:0))
+   return arr;
+} 
