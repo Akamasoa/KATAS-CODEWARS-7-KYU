dfa05c0a5b00cbd501428dadb238cec635841060
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-02 22:32:32 -0300

    Update Maximum Gap (Array Series #4).js

--- Maximum Gap (Array Series #4).js
+++ Maximum Gap (Array Series #4).js
@@ -44,3 +44,12 @@ Enjoy Learning !!
 Zizou
 */
 
+function maxGap(numbers) {
+  let res = [];
+  let arr = numbers
+    .sort((a, b) => b - a)
+    .map((v, i, arr) => {
+      res.push(arr[i + 1] - arr[i]);
+    });
+  return Math.abs(res.filter(v => !isNaN(v)).sort((a, b) => a - b)[0]);
+}
