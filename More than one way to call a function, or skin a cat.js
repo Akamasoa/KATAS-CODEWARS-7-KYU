b23de9722484ecae2d91ee60040c5d0d5e2405e6
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-10 21:44:37 -0300

    Update More than one way to call a function, or skin a cat.js

--- More than one way to call a function, or skin a cat.js
+++ More than one way to call a function, or skin a cat.js
@@ -11,3 +11,15 @@ clojure (sum 2 3) ;; 5 ;;or ((sum 2) 3) ;; 5
 Both of these examples should return the sum of the 2 numbers.
 */
 
+function sum(a, b) {
+  if (arguments.length === 1)
+    return function(b) {
+      return a + b;
+    };
+  return a + b;
+}
+
+/*
+Short solution:
+const sum = (a,b) => b !== undefined ? a + b : (b) => a + b;
+*/
