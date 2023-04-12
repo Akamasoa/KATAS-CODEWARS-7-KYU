7810feee1a93061913024d11b3361c647f329d85
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-11 22:21:55 -0300

    Update Move 10.js

--- Move 10.js
+++ Move 10.js
@@ -7,3 +7,16 @@ If it goes past 'z', start again at 'a'.
 Input will be a string with length > 0.
 */
 
+function moveTen(s) {
+  const str = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+  const arr = s.split("").map(v => (v = str.indexOf(v) + 10));
+  return arr.map(v => (v = str[v])).join("");
+}
+
+/*
+function moveTen(s){
+  return s.split('').map(e => e.charCodeAt(0) + 10 > 122 
+                                ? String.fromCharCode(e.charCodeAt(0) - 16) 
+                                : String.fromCharCode(e.charCodeAt(0) + 10)).join('');
+}
+*/
