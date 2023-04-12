86ded4b485004f946c66008a031fc35f580a8718
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-11 22:12:08 -0300

    Update Most digits.js

--- Most digits.js
+++ Most digits.js
@@ -5,3 +5,21 @@ Find the number with the most digits.
 If two numbers in the argument array have the same number of digits, return the first one in the array.
 */
 
+function findLongest(array){
+  return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
+}
+
+/*
+Long solution:
+
+function findLongest(array){
+  let num = '0';
+  for(let i = 0; i < array.length; i++) {
+    let string = array[i].toString();
+    if(string.length > num.length) {
+      num = string;
+    }
+  }
+  return parseInt(num);
+}
+*/
