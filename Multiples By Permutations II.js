de6dfd3714aeb2dde6f099d81f414acbb542173a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-13 20:49:58 -0300

    Update Multiples By Permutations II.js

--- Multiples By Permutations II.js
+++ Multiples By Permutations II.js
@@ -31,3 +31,11 @@ Features of the random tests
 Enjoy it!!
 */
 
+function findLowestInt(k) {
+  let n = 1;
+  const x = k + 1;
+  const string = n => n.toString().split``.sort().join``;
+  while (1) 
+    if (string(n * k) === string(n++ * x)) 
+      return --n;
+}
