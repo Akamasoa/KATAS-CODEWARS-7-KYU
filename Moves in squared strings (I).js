71327d94a4bfad8a2bb0c188492f83d2c1f37918
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-13 20:45:48 -0300

    Update Moves in squared strings (I).js

--- Moves in squared strings (I).js
+++ Moves in squared strings (I).js
@@ -38,3 +38,22 @@ Bash Note:
 The input strings are separated by , instead of \n. The output strings should be separated by \r instead of \n. See "Sample Tests".
 */
 
+function vertMirror(str) {
+    return str.split('\n')
+      .map(line => line.split('').reverse().join(''))
+      .join('\n')
+}
+function horMirror(str) {
+    return str.split('\n')
+      .reverse()
+      .join('\n')
+}
+function oper(fct, s) {
+    return fct(s)
+}
+
+/*
+const vertMirror = s => s.map(s => [...s].reverse().join(''));
+const horMirror = s => s.reverse();
+const oper = (fct, s) => fct(s.split("\n")).join("\n");
+*/
