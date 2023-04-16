160a969747ffc0ccf636131b74591657fe4b90e7
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-15 23:01:07 -0300

    Update Mumbling.js

--- Mumbling.js
+++ Mumbling.js
@@ -9,3 +9,9 @@ accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 */
 
+function accum(s) {
+  return s
+    .split("")
+    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+    .join("-");
+}
