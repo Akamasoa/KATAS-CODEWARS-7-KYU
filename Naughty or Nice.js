7563210d9bf4fca36cfd08fc92487a3d0f36bd95
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-18 23:00:05 -0300

    Update Naughty or Nice.js

--- Naughty or Nice.js
+++ Naughty or Nice.js
@@ -16,3 +16,14 @@ Santa is coming to town and he needs your help finding out who's been naughty or
 Your function should return "Naughty!" or "Nice!" depending on the total number of occurrences in a given year (whichever one is greater). If both are equal, return "Nice!"
 */
 
+function charConcat(string){
+  let str = string.split('');
+  
+  let arr = [];
+  for (let i = 1;i <= str.length / 2; i++){
+  arr.push(str[i - 1]);
+   arr.push(str[str.length - i])
+      arr.push(i);
+  }
+  return arr.join('');
+}
