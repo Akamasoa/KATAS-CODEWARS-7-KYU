8af5c18dc0dd503dfa0a230de2bf4ea8f2af8552
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-21 18:31:18 -0300

    Update No ifs no buts.js

--- No ifs no buts.js
+++ No ifs no buts.js
@@ -19,3 +19,10 @@ in fact the word "if" and the character "?" are not allowed in the code.
 Inputs are guarenteed to be numbers
 */
 
+var noIfsNoButs = function (a,b) {
+  switch (Math.sign(a-b)) {
+    case 1: return `${a} is greater than ${b}`;
+    case -1:return `${a} is smaller than ${b}`;
+    case 0: return `${a} is equal to ${b}`;
+  }
+}
