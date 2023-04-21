e2842fdf7e473da64b13276d567173a263de9d43
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-20 21:53:41 -0300

    Update Next birth-day of the week finder.js

--- Next birth-day of the week finder.js
+++ Next birth-day of the week finder.js
@@ -12,3 +12,15 @@ nextBirthdayOfTheWeek(new Date(2012, 5, 20))  //6
 nextBirthdayOfTheWeek(new Date(1975, 2, 22))  //5
 */
 
+let nextBirthdayOfTheWeek = function(birthday) {
+  let weekday = birthday.getDay();
+  let year = birthday.getFullYear();
+  year += 1;
+  let month = birthday.getMonth();
+  let day = birthday.getDate();
+
+  while (weekday !== new Date(year, month, day).getDay()) {
+    year++;
+  }
+  return year - birthday.getFullYear();
+};
