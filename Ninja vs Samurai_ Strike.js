2451f62c175e9e8de4d98778d3f1b82ebb655b62
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-22 20:43:18 -0300

    Update Ninja vs Samurai_ Strike.js

--- Ninja vs Samurai_ Strike.js
+++ Ninja vs Samurai_ Strike.js
@@ -9,3 +9,11 @@ samurai.strike(ninja, 3);
 Can you figure out what is wrong?
 */
 
+let Warrior = function(name){
+  this.name = name;  
+  this.health = 100;
+}
+
+Warrior.prototype.strike = function(enemy, swings){
+  enemy.health = Math.max(0, enemy.health - (swings * 10));   
+}
