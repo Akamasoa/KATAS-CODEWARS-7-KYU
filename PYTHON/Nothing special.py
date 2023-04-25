6e21e4d3b0be8f7efc68cb7104737f54a757b2c0
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-24 23:13:40 -0300

    Update Nothing special.py

--- PYTHON/Nothing special.py
+++ PYTHON/Nothing special.py
@@ -5,3 +5,15 @@ The notorious Captain Schneider has given you a very straight forward mission. A
 Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
 """
 
+def nothing_special(s):
+    # Verifico si la entrada es una cadena de texto:
+    if not isinstance(s, str):
+        return "Not a string!"
+    
+    # Creo una cadena de caracteres permitidos que incluya letras mayúsculas y minúsculas, dígitos y espacios en blanco:
+    allowed_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 "
+    
+    # Filto la cadena original para mantener sólo los caracteres permitidos:
+    filtered_string = ''.join(c for c in s if c in allowed_chars)
+    
+    return filtered_string
