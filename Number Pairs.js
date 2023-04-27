72e260fabd156f1c28bfaf2edfb8f0a45d2247de
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-26 23:37:58 -0300

    Update Number Pairs.js

--- Number Pairs.js
+++ Number Pairs.js
@@ -11,3 +11,16 @@ let arr2 = [23, 14, 53, 17, 80];
 getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
 */
 
+function getLargerNumbers(a, b) {
+  return a.map((v, i, arr) => (arr[i] > b[i] ? arr[i] : b[i]));
+}
+
+/*
+function getLargerNumbers(a, b) {
+  let newArray = [];
+  for (let i = 0; i < a.length; i++) {
+    newArray.push( Math.max( a[i], b[i] ) );
+  }
+  return newArray;
+}
+*/
