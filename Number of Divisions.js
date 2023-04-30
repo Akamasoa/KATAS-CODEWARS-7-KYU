5f48b2791952cb2ff4d90414c25149c578ae1d42
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-30 20:44:37 -0300

    Update Number of Divisions.js

--- Number of Divisions.js
+++ Number of Divisions.js
@@ -17,3 +17,11 @@ For example the number 6 can be divided by 2 two times:
 6. 3 / 2 = 1 remainder 1
 */
 
+const divisions = (n, divisor) => {
+  let count = 0;
+  while (n > 1) {
+    count++;
+    n = n / divisor;
+  }
+  return count - 1;
+};
