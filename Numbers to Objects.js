9188bbf41b5905b6dd6c69a6ac653e364dd4f912
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-05 18:45:13 -0300

    Update Numbers to Objects.js

--- Numbers to Objects.js
+++ Numbers to Objects.js
@@ -11,3 +11,20 @@ Return an array of the resulting objects.
 All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
 */
 
+function numObj(s){
+  return s.map(n => {
+    return { [n]: String.fromCharCode(n) };
+  });
+}
+
+/*
+function numObj(s){
+  const arr = []
+  for(i=0; i<s.length;i++){
+    const obj = {}
+    obj[s[i]] = String.fromCharCode(s[i])
+    arr.push(obj)
+  }
+  return arr
+}
+*/
