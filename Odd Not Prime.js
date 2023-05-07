a6d3ca6225b81510a7f4fb21600272eccc75486d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-07 18:59:06 -0300

    Update Odd Not Prime.js

--- Odd Not Prime.js
+++ Odd Not Prime.js
@@ -7,3 +7,19 @@ Example: 5 has three odd numbers (1,3,5) and only the number 1 is not prime, so 
 Example: 10 has five odd numbers (1,3,5,7,9) and only 1 and 9 are not prime, so the answer is 2
 */
 
+function oddNotPrime(n){
+  let count = 1;
+  for (let i = 1;i <= n; i += 2)
+  {
+    let c = 0;
+    for (let j=2; j<i; ++j)
+      if (i % j == 0)
+      {
+        c++;
+        break;
+      }
+    if (c != 0)
+      count++;
+  }
+  return count;
+}
