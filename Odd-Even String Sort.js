401dda9d4850d50149146f5f1e5a7949944d3459
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-09 22:47:04 -0300

    Update Odd-Even String Sort.js

--- Odd-Even String Sort.js
+++ Odd-Even String Sort.js
@@ -23,3 +23,16 @@ And the final string to return is 'Cdwr oeas'
 Enjoy.
 */
 
+function sortMyString(S) {
+  let strEven = ''
+  let strOdd = ''
+  for (let i = 0; i < S.length; i++) {
+    if (i % 2 === 0) {
+      strEven += S[i]
+    }
+    else {
+      strOdd += S[i]
+    }
+  }
+  return strEven + ' ' + strOdd
+}
