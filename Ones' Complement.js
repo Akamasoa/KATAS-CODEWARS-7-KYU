4a2d3a5f966075347ca80b5768249df29dc0ca7b
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-12 20:48:44 -0300

    Update Ones' Complement.js

--- Ones' Complement.js
+++ Ones' Complement.js
@@ -7,3 +7,20 @@ onesComplement(1001) = 0110
 For any given binary number,formatted as a string, return the Ones' Complement of that number.
 */
 
+function onesComplement(n) {
+  return n.replace(/[01]/gi, v => (v === "1" ? (v = "0") : (v = "1")));
+}
+
+/*
+function onesComplement(n) {
+  let result = '';
+  for (let i of n){
+    if(i == '1'){
+      result += '0';
+    }else{
+      result += '1';
+    }
+  }
+  return result;
+}
+*/
