db569d09ca9a9843765326d0005c39d636a5a026
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-14 23:01:01 -0300

    Update Operator overload?.js

--- Operator overload?.js
+++ Operator overload?.js
@@ -8,3 +8,10 @@ We want to be able to sum two Foo object using the '+' operator.
 foo + boo // => must return 5
 */
 
+var Foo = function(value) {
+  this.val = value;
+};
+
+Foo.prototype.valueOf = function() {
+  return this.val;
+};
