22612cdc27029fb58f6c87a260eb56ba14d1708a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-17 22:31:17 -0300

    Create Paperboy 2.js

---
+++ Paperboy 2.js
@@ -0,0 +1,17 @@
+/*
+Description:
+Given a neighborhood represented by a string in Python, where each character represents a house, an obstacle, or the location of the paperboy, write a function that returns the number of houses that the paperboy has delivered to, taking into account that he can only deliver papers to houses that are on his route and that he must avoid obstacles on the way.
+
+The string has the following structure:
+
+"." represents a house that needs papers.
+"+" represents the location of the paperboy.
+"-" represents an empty street.
+"|" represents an obstacle.
+The string has the following format:
+
+"*\n-----\n*-+---\n--|---\n--|-.<\n-----\n**"
+
+The string can be read from top to bottom and from left to right. The paperboy starts at the location indicated by "+". The goal is to deliver papers to all houses indicated by ".", avoiding obstacles indicated by "|", and reaching the finish line indicated by "<".
+*/
+
