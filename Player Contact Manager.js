a1a6311b01457c21483bfe4821a6b0e627c738b8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-25 23:03:59 -0300

    Update Player Contact Manager.js

--- Player Contact Manager.js
+++ Player Contact Manager.js
@@ -16,3 +16,12 @@ playerManager("")   returns []
 Have at thee!
 */
 
+function playerManager(players) {
+  if(players === '' || players === null) return [];
+  players = players.split`, `;
+  let result = [];
+  for(let i = 0; i < players.length; i+=2) {
+    result.push( {player: players[i], contact: +players[i+1]} );
+  }
+  return result;
+}
