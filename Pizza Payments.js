bddde64e4ccbb3bd1b12d0f103e45669b3699045
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-25 23:01:00 -0300

    Update Pizza Payments.js

--- Pizza Payments.js
+++ Pizza Payments.js
@@ -7,3 +7,19 @@ Otherwise Kate will contribute 1/3 of the price, but no more than €10 (she's b
 How much is Michael going to pay? Calculate the amount with two decimals, if necessary.
 */
 
+function michaelPays(costs) {
+  let ans = 0;
+  if (costs < 5) {
+    ans = costs
+  } else {
+    let third = costs/3;
+    third > 10 
+      ? ans = costs - 10
+      : ans = third * 2;
+  }
+  return Math.round(ans * 100) / 100;
+}
+
+/*
+michaelPays = c => + Math.max(c < 5 ? c : 2 / 3 * c, c - 10).toFixed(2);
+*/
