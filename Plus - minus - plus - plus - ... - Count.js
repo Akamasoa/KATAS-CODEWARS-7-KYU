bf217f38a8315d3c6749aa758d1bb8e8288a2b84
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-27 20:53:41 -0300

    Update Plus - minus - plus - plus - ... - Count.js

--- Plus - minus - plus - plus - ... - Count.js
+++ Plus - minus - plus - plus - ... - Count.js
@@ -21,3 +21,11 @@ const arr = [1, -3, -4, 0, 5];
 catchSignChange(arr) == 2;
 */
 
+const catchSignChange = arr => {
+  let count = 0;
+  let a = arr.map((v, i, arr) =>
+    (v >= 0 && arr[i + 1] < 0) || (v < 0 && arr[i + 1] >= 0) ? count++ : count
+  );
+
+  return count;
+};
