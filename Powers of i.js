fb52f7e3306062e2b111c8187040bdfeab0aee07
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-30 20:04:57 -0300

    Update Powers of i.js

--- Powers of i.js
+++ Powers of i.js
@@ -24,3 +24,15 @@ i to the power of a given non-negative integer in its simplest form, as a string
 i).
 */
 
+function pofi(n) {
+  const remainder = n % 4;
+  if (remainder === 0) {
+    return "1";
+  } else if (remainder === 1) {
+    return "i";
+  } else if (remainder === 2) {
+    return "-1";
+  } else {
+    return "-i";
+  }
+}
