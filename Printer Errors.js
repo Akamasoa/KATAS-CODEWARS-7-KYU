358062584fba1762e9a4f8f29d48144757a09518
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-05-31 22:06:50 -0300

    Update Printer Errors.js

--- Printer Errors.js
+++ Printer Errors.js
@@ -18,3 +18,12 @@ s="aaaxbbbbyyhwawiwjjjwwm"
 printer_error(s) => "8/22"
 */
 
+function printerError(s) {
+    let count = 0;
+    for(let i = 0; i < s.length; i++) {
+      if (s[i] > "m") {
+        count++;
+      }
+    }
+    return count+"/"+s.length;
+}
