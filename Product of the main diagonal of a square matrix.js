0b08297e1f43c738e611195b029b370d0ea33486
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-03 22:06:53 -0300

    Create Product of the main diagonal of a square matrix.js

---
+++ Product of the main diagonal of a square matrix.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Given a list of rows of a square matrix, find the product of the main diagonal.
+
+Examples:
+
+mainDiagonalProduct([[1,0],[0,1]]) => 1
+
+mainDiagonalProduct([[1,2,3],[4,5,6],[7,8,9]]) => 45
+http://en.wikipedia.org/wiki/Main_diagonal
+*/
+
