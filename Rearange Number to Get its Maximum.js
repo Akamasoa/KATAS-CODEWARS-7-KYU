cef2c3920aac93421e47ca014b0f5ed7726730bd
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-07 22:38:12 -0300

    Create Rearange Number to Get its Maximum.js

---
+++ Rearange Number to Get its Maximum.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Create a function that takes one positive three digit integer and rearranges its digits to get the maximum possible number. Assume that the argument is an integer.
+
+Return -1 if the argument is not valid
+Return null (nil for Ruby, nothing for Julia) if the argument is not valid.
+
+maxRedigit(123); // returns 321
+*/
+
