34ddcba307aef0c0635d785c8bd0cd3a100c28e2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-07 22:48:46 -0300

    Update Rearange Number to Get its Maximum.js

--- Rearange Number to Get its Maximum.js
+++ Rearange Number to Get its Maximum.js
@@ -8,3 +8,12 @@ Return null (nil for Ruby, nothing for Julia) if the argument is not valid.
 maxRedigit(123); // returns 321
 */
 
+var maxRedigit = function(num) {
+  if (("" + num).length !== 3) return null;
+  return (
+    ("" + num)
+      .split("")
+      .sort((a, b) => b - a)
+      .join("") * 1
+  );
+};
