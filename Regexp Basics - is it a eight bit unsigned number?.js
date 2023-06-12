b7cd4491daf4402658656e6ebfdfc9f52220d854
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-11 22:46:13 -0300

    Update Regexp Basics - is it a eight bit unsigned number?.js

--- Regexp Basics - is it a eight bit unsigned number?.js
+++ Regexp Basics - is it a eight bit unsigned number?.js
@@ -5,3 +5,8 @@ Implement String#eight_bit_number?, which should return true if given object is 
 It should only accept numbers in canonical representation, so no leading +, extra 0s, spaces etc.
 */
 
+String.prototype.signedEightBitNumber = function() {
+  return (
+    Number(this).toString() == this && Number(this) > -129 && Number(this) < 128
+  );
+};
