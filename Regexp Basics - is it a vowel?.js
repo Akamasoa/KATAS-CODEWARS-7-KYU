16a976c690197ab8c76db2e205973fb17f32a456
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-13 20:52:08 -0300

    Update Regexp Basics - is it a vowel?.js

--- Regexp Basics - is it a vowel?.js
+++ Regexp Basics - is it a vowel?.js
@@ -3,3 +3,12 @@ DESCRIPTION:
 Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
 */
 
+String.prototype.vowel = function() {
+  return this.match(/[aeiou]/gi) && this.length <= 1 ? true : false;
+};
+
+/*
+String.prototype.vowel = function() {
+  return /^[aeiou]$/i.test(this)
+}
+*/
