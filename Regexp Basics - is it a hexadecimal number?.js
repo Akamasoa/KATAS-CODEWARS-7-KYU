d58255a3e914b75d0c7db4a53c58504322d4a019
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-12 22:17:51 -0300

    Create Regexp Basics - is it a hexadecimal number?.js

---
+++ Regexp Basics - is it a hexadecimal number?.js
@@ -0,0 +1,7 @@
+/*
+DESCRIPTION:
+Implement String#hex_number? (in Java StringUtils.isHexNumber(String)), which should return true if given object is a hexadecimal number, false otherwise.
+
+Hexadecimal numbers consist of one or more digits from range 0-9 A-F (in any case), optionally prefixed by 0x.
+*/
+
