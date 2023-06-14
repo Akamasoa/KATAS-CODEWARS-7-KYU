e2b6ac6b5c3b7c99236e2fe282b2c7b950fbf489
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-13 21:57:40 -0300

    Update Regexp basics - parsing prices.js

--- Regexp basics - parsing prices.js
+++ Regexp basics - parsing prices.js
@@ -3,3 +3,6 @@ DESCRIPTION:
 Implement String#to_cents, which should parse prices expressed as $1.23 and return number of cents, or in case of bad format return nil.
 */
 
+String.prototype.toCents = function () {
+  return /^\$\d+\.\d\d$/.test(this) ? +this.replace(/[$.]/g, '') : null;
+};
