f5708022d2515f32e4e5ee0bdaedacd5383e9873
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-13 22:09:11 -0300

    Update Remove All The Marked Elements of a List.js

--- Remove All The Marked Elements of a List.js
+++ Remove All The Marked Elements of a List.js
@@ -9,3 +9,8 @@ Examples (input -> output):
 Enjoy it!!
 */
 
+Array.prototype.remove_ = function(integer_list, values_list){
+	return integer_list.filter(function (element) {
+  	return values_list.indexOf(element) === -1;
+  });
+}
