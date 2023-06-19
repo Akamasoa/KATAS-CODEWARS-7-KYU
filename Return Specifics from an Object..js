532a2b3230cc4ed1052cfad83d3cc995211c8281
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-18 21:03:46 -0300

    Update Return Specifics from an Object..js

--- Return Specifics from an Object..js
+++ Return Specifics from an Object..js
@@ -11,3 +11,11 @@ returnSpecifics({a:1,b:'str',c:2,d:true,e:function(a){return a;},f:3})
 This would return [1,2,3,'e'] 
 */
 
+const returnSpecifics = o => {
+  let [N, F] = [[], []];
+  for (let k in o) {
+    if (typeof o[k] === "number") N.push(o[k]);
+    if (typeof o[k] === "function") F.push(k);
+  }
+  return N.length < 1 && F.length < 1 ? ["The Object is Empty"] : [...N, ...F];
+};
