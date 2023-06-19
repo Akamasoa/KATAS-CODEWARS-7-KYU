3f40adae62bf111010001d5882a51b2512904019
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-19 18:53:22 -0300

    Update Return a string's even characters..js

--- Return a string's even characters..js
+++ Return a string's even characters..js
@@ -8,3 +8,13 @@ For example:
 "a"             --> "invalid string"
 */
 
+function evenChars(str) {
+  if (str.length < 2 || str.length > 100) {
+    return "invalid string";
+  }
+  let evenCharsArr = [];
+  for (let i = 1; i < str.length; i += 2) {
+    evenCharsArr.push(str[i]);
+  }
+  return evenCharsArr;
+}
