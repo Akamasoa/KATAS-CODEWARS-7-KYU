9565d423cf70d78caef053f24e1970891d7e87f7
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-18 21:06:22 -0300

    Update Return String of First Characters.js

--- Return String of First Characters.js
+++ Return String of First Characters.js
@@ -8,3 +8,9 @@ For example:
 Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
 */
 
+function makeString(s) {
+  return s
+    .split(" ")
+    .map(v => v.slice(0, 1))
+    .join("");
+}
