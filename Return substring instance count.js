ba8a27a07491540b32cd3ceb433802559fcc4f61
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-20 23:32:03 -0300

    Update Return substring instance count.js

--- Return substring instance count.js
+++ Return substring instance count.js
@@ -8,3 +8,19 @@ solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
 solution('aaabbbcccc', 'bbb') # should return 1
 */
 
+function solution(fullText, searchText){ 
+  let array1 = (fullText.split(searchText));
+  //console.log(array1);
+    let result = 0;
+    for (let i = 0; i < array1.length; i++) {
+        result ++;
+      //console.log(result);
+    }
+    return (result - 1)
+}
+
+/*
+function solution(fullText, searchText){
+  return fullText.split(searchText).length - 1;
+}
+*/
