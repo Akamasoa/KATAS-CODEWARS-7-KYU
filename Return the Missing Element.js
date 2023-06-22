b7e6978576dd293dea326a921a1fb7169871fc30
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-21 21:50:35 -0300

    Update Return the Missing Element.js

--- Return the Missing Element.js
+++ Return the Missing Element.js
@@ -11,3 +11,11 @@ Examples:
 [9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
 */
 
+function getMissingElement(superImportantArray) {
+  let count = 0;
+  let arr = superImportantArray.sort((a, b) => a - b);
+  for (let i = 0; i <= arr.length; i++) {
+    if (i == arr[i]) count++;
+  }
+  return count;
+}
