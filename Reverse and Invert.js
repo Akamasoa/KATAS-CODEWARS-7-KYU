94ab89fdc7998d3d90ab2050bf6f4019234cf079
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-23 20:53:51 -0300

    Update Reverse and Invert.js

--- Reverse and Invert.js
+++ Reverse and Invert.js
@@ -6,3 +6,8 @@ reverse_invert([1,12,'a',3.4,87,99.9,-42,50,5.6]) = [-1,-21,-78,24,-5]
 Remove all types other than integer.
 */
 
+function reverseInvert(array) {
+  return array
+    .filter(n => Number.isInteger(n))
+    .map(n => -Math.sign(n) * parseInt(String(n).split("").reverse().join(""), 10))
+}
