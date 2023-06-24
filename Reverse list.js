72d6497a89dcbbf0094d1ef9c58839f21416bf9c
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-24 19:33:24 -0300

    Update Reverse list.js

--- Reverse list.js
+++ Reverse list.js
@@ -6,3 +6,14 @@ Write reverseList function that simply reverses lists.
 function reverseList(arr) {
   return arr.reverse();
 }
+
+ /*
+ The long way:
+ 
+ function reverseList(arr) {
+  const reverseArray = [];
+  for(let i = arr.length - 1; i >= 0; i--)
+    reverseArray.push(arr[i]);
+  return reverseArray;
+}
+*/
