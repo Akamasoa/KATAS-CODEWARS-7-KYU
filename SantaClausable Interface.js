71efbae8f074552cec8570153816dcf3f29ff139
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-27 22:08:55 -0300

    Update SantaClausable Interface.js

--- SantaClausable Interface.js
+++ SantaClausable Interface.js
@@ -26,3 +26,10 @@ Additional Information on this Topic
 Duck Typing (Wikipedia)
 */
 
+function isSantaClausable(obj) {
+  return ["sayHoHoHo", "distributeGifts", "goDownTheChimney"].every(function(
+    methodName
+  ) {
+    return typeof obj[methodName] == "function";
+  });
+}
