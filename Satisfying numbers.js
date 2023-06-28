e596b5677429290044fc7662d3d43420152112ba
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-27 22:15:13 -0300

    Update Satisfying numbers.js

--- Satisfying numbers.js
+++ Satisfying numbers.js
@@ -12,3 +12,17 @@ smallest(5) == 60 // 1 to 5 can all divide evenly into 60
 smallest(10) == 2520
 */
 
+function smallest(n) {
+  let res = n * (n - 1) || 1;
+  for (let i = n - 1; i >= 1; i--) {
+    if (res % i) {
+      for (let j = n - 1; j >= 1; j--) {
+        if (!(i % j) && !(res % j)) {
+          res = i * res / j;
+          break;
+        }
+      }
+    }
+  }
+  return res;
+}
