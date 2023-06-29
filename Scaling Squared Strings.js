22fb21beaed354f885ce6d2eca38460bf022789e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-29 19:33:49 -0300

    Update Scaling Squared Strings.js

--- Scaling Squared Strings.js
+++ Scaling Squared Strings.js
@@ -34,3 +34,17 @@ Task:
 Write function scale(strng, k, v) k and v will be positive integers. If strng == "" return "".
 */
 
+function scale(strng, k, n) {
+  const arr = strng
+    .split("\n")
+    .map(v =>
+      v
+        .split("")
+        .map(v => v.repeat(k))
+        .join("")
+    )
+    .map(v => (v + " ").repeat(n))
+    .map(v => v.trim().replace(/ /g, "\n"))
+    .join("\n");
+  return arr;
+}
