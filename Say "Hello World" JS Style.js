79b219d34349777bd05faa149a38d54a19e115fd
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-06-28 21:27:35 -0300

    Update Say "Hello World" JS Style.js

--- Say "Hello World" JS Style.js
+++ Say "Hello World" JS Style.js
@@ -5,3 +5,9 @@ Write the definition of the function "say" such that calling this:
 say("Hello")("World")
 returns "Hello World"
 */
+
+var say = function(str1) {
+  return function(str2) {
+    return `${str1} ${str2}`;
+  };
+};
