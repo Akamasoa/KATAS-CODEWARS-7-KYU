eb2bd097ad094c06aa4554a4048f449b1f923cc9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-02 22:45:59 -0300

    Update See You Next Happy Year.js

--- See You Next Happy Year.js
+++ See You Next Happy Year.js
@@ -35,3 +35,13 @@ Zizou
 FUNDAMENTALS
 */
 
+function nextHappyYear(year) {
+  let answer = 0;
+  for (let i = year + 1; ; i++) {
+    if ([...new Set(("" + i).split(""))].length === 4) {
+      answer = i;
+      break;
+    }
+  }
+  return answer;
+}
