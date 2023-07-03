0fdff540b27b2c15dfab33e03701dc04d0e733e9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-03 11:50:15 -0300

    Update Sentence to words.js

--- Sentence to words.js
+++ Sentence to words.js
@@ -7,3 +7,24 @@ Example:
 "hello world" -> ["hello", "world"]
 */
 
+function splitSentence(s) {
+  return s.split(' ');
+}
+
+/*
+function splitSentence(s) {
+  var array = [];
+  var string = '';
+  for(i in s) {
+    if(s[i] == ' ') {
+      array.push(string);
+      string = '';
+    }
+    else {
+      string = string.concat(s[i]);
+    }
+  }
+  array.push(string);
+  return array;
+}
+*/
