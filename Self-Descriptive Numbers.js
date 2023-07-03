22eb365ab017a33f5a734996e86173a5a7d96757
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-03 11:41:58 -0300

    Update Self-Descriptive Numbers.js

--- Self-Descriptive Numbers.js
+++ Self-Descriptive Numbers.js
@@ -17,3 +17,17 @@ There are no 4's in the number, so the fifth digit is 0
 Numbers can be of any length up to 9 digits and are only full integers. For a given number derive a function selfDescriptive(num) that returns; true if the number is self-descriptive or false if the number is not.
 */
 
+function selfDescriptive(num) {
+  let str = '' + num;
+  
+  let counts = str.split('').reduce(function(counts, digit) {
+    counts[digit] = (counts[digit] || 0) + 1;
+    return counts;
+  }, []);
+  
+  for (let i = 0; i < str.length; i++) 
+    if ((counts[i] || 0) != str[i]) 
+      return false;
+
+  return true;
+}
