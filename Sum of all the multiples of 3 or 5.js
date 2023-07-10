1d9e9d7cea4b1fbd6cd301fea6bf5d04548203cf
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-09 21:19:10 -0300

    Update Sum of all the multiples of 3 or 5.js

--- Sum of all the multiples of 3 or 5.js
+++ Sum of all the multiples of 3 or 5.js
@@ -1,12 +1,14 @@
 /*
 DESCRIPTION:
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Your task is to write function findSum.
 
-Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
 
-Note: If the number is a multiple of both 3 and 5, only count it once.
+For example:
 
-Courtesy of projecteuler.net (Problem 1)
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
 */
 
 function findSum(n) {
