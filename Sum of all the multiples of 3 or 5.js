bcf425273fdd36aa24f6b281e46a95a069b67c06
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-09 21:16:00 -0300

    Update Sum of all the multiples of 3 or 5.js

--- Sum of all the multiples of 3 or 5.js
+++ Sum of all the multiples of 3 or 5.js
@@ -9,3 +9,10 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 Courtesy of projecteuler.net (Problem 1)
 */
 
+function findSum(n) {
+  let result = 0;
+  for (let i = 0; i <= n; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) result += i;
+  }
+  return result;
+}
