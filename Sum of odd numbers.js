a63558a3666dea5e9ffec84cadb3fd51c4240411
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-12 22:38:43 -0300

    Update Sum of odd numbers.js

--- Sum of odd numbers.js
+++ Sum of odd numbers.js
@@ -14,3 +14,12 @@ Calculate the sum of the numbers in the nth row of this triangle (starting at in
 2 --> 3 + 5 = 8
 */
 
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+/*
+function rowSumOddNumbers(n) {
+	return n**3
+}
+*/
