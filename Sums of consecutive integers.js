143da9451f6cf4ed430dbdf88f660434cddcdef6
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-16 19:05:14 -0300

    Update Sums of consecutive integers.js

--- Sums of consecutive integers.js
+++ Sums of consecutive integers.js
@@ -14,6 +14,15 @@ SumOfConsecutiveIntegers.Position(4, 14, 3) => 5
 Assume there will always be a sum of x consecutive integers that totals to y and n will never be indexed out of bounds.
 */
 
+/**
+ * Given the number of consecutive integers and the total of the integers,
+ * return the consecutive integer at the requested position.
+ *
+ * @param {int} x number of consecutive integers
+ * @param {int} y sum of consecutive integers
+ * @param {int} n position of requested integer
+ * @return {int} consecutive integer at requested position
+ */
 function position(x, y, n) {
   return y / x - (x - 1) / 2 + n;
 }
