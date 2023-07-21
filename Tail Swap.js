50a83fa52e51d896c6c4a62d39f44f647ca4e72a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-20 23:18:50 -0300

    Update Tail Swap.js

--- Tail Swap.js
+++ Tail Swap.js
@@ -9,3 +9,13 @@ Examples
 ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
 */
 
+function tailSwap(arr) {
+  let head = [];
+  let tail = [];
+  arr.map(v =>
+    v.split(":").map((v, i, arr) => (i % 2 == 0 ? head.push(v) : tail.push(v)))
+  );
+  tail = tail.reverse();
+  head = head.map((v, i) => v + ":" + tail[i]);
+  return head;
+}
