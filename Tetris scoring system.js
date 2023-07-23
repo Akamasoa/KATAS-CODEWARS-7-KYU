ec1b8746ce4dce113045ef1bd4d909032ee7d0a8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-23 20:22:16 -0300

    Update Tetris scoring system.js

--- Tetris scoring system.js
+++ Tetris scoring system.js
@@ -58,3 +58,29 @@ Tetris Series #4 — New Block Types (TBA)
 Tetris Series #5 — Complex Block Types (TBA?)
 */
 
+function getScore(arr) {
+  let mult = 1;
+  let lines = 0;
+  let score = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      if (arr[i] === 1) {
+        score += 40 * mult;
+      }
+      if (arr[i] === 2) {
+        score += 100 * mult;
+      }
+      if (arr[i] === 3) {
+        score += 300 * mult;
+      }
+      if (arr[i] === 4) {
+        score += 1200 * mult;
+      }
+      lines += arr[i];
+      if (lines >= mult * 10) {
+        mult += 1;
+      }
+    }
+  }
+  return score;
+}
