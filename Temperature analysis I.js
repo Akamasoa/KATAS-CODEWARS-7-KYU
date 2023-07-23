788974302e9a0a9dca610ebe26fa9525f0681cbb
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-22 21:33:02 -0300

    Update Temperature analysis I.js

--- Temperature analysis I.js
+++ Temperature analysis I.js
@@ -6,3 +6,19 @@ You were given a string of integer temperature values. Create a function lowest_
 function lowestTemp(t) {
   return t.length > 0 ? Math.min(...t.split(" ").map(Number)) : null;
 }
+
+/*
+function lowestTemp(t) {
+  let arr = [];
+  let str = "";
+  for (let i = 0; i < t.length; i++) {
+    while (t[i] !== " " && i < t.length) {
+      str += t[i];
+      i++;
+    }
+    arr.push(+str);
+    str = "";
+  }
+  return t.length > 0 ? Math.min(...arr) : null;
+}
+*/
