d19c36a3a429c06c7e0c84d1ed5c7aba420b7b85
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-24 18:52:18 -0300

    Create The Arpeggiator.js

---
+++ The Arpeggiator.js
@@ -0,0 +1,32 @@
+/*
+DESCRIPTION:
+In music, a scale is made up of seven different values (A through G inclusive) with the ROOT value occuring twice forming the scale's first and eighth (last) note.
+
+So if the root = B, the scale looks like this:
+
+  B C D E F G A B
+  
+and if the root = F, the scale looks like this:
+
+  F G A B C D E F
+  
+Each scale has an arpeggio made up of it's first, third, fifth and eighth notes.
+
+So the arpeggio for scale root B looks like this:
+
+ B D F B
+ 
+and the arpeggio for scale root F looks like this:
+
+ F A C F
+ 
+Write a programme that takes any possible root note and returns it's arpeggio in an array. If the object input isn't a root note, return 'undefined/Nothing'. Watch for capitalization!
+
+Example:
+
+arpeggio 'A' == Just "ACEA"
+arpeggio 'E' == Just "EGBE"
+arpeggio 'Z' == Nothing
+arpeggio 'a' == Nothing
+*/
+
