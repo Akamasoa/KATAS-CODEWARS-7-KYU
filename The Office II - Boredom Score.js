e09ca0618cc4c9306d5898500cd9e787dab73d8e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-27 20:41:32 -0300

    Update The Office II - Boredom Score.js

--- The Office II - Boredom Score.js
+++ The Office II - Boredom Score.js
@@ -29,3 +29,24 @@ The Office IV - Find a Meeting Room
 The Office V - Find a Chair
 */
 
+function boredom(staff) {
+  const arr = Object.values(staff).reduce((a, b) => a + obj[b], 0);
+  return arr <= 80
+    ? "kill me now"
+    : arr >= 100
+    ? "party time!!"
+    : "i can handle this";
+}
+
+const obj = {
+  accounts: 1,
+  finance: 2,
+  canteen: 10,
+  regulation: 3,
+  trading: 6,
+  change: 6,
+  IS: 8,
+  retail: 5,
+  cleaning: 4,
+  "pissing about": 25
+};
