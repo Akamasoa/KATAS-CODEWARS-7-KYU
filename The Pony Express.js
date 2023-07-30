8bce5ffc9d607053104e9665e1e3dc02aa3ccd46
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-07-30 18:43:47 -0300

    Update The Pony Express.js

--- The Pony Express.js
+++ The Pony Express.js
@@ -28,3 +28,17 @@ The Pony Express
 The Pony Express (missing rider)
 */
 
+function riders(stations) {  
+  let rider = 1
+  let distance = 0
+  for(s of stations){
+    if(distance + s > 100){
+      rider += 1
+      distance = s
+    }
+    else {
+      distance += s
+    }
+  }
+  return rider
+}
