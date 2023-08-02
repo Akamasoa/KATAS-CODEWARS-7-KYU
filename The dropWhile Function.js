85349fcd44e81d1cd923e62303b6d1a4a4d45ea8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-01 23:38:17 -0300

    Update The dropWhile Function.js

--- The dropWhile Function.js
+++ The dropWhile Function.js
@@ -11,3 +11,7 @@ dropWhile(seq, isEven) // -> [1,2,5,4,3,2]
 Your task is to implement the dropWhile function. If you've got a span function lying around, this is a one-liner! Alternatively, if you have a takeWhile function on your hands, then combined with the dropWhile function, you can implement the span function in one line. This is the beauty of functional programming: there are a whole host of useful functions, many of which can be implemented in terms of each other.
 */
 
+function dropWhile(arr, pred) {
+  while (arr.length && pred(arr[0])) arr.shift()
+  return arr;
+}
