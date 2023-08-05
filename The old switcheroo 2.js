ab6c831598e56d3e017cc366099e11c17e5a5f51
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-05 20:36:43 -0300

    Update The old switcheroo 2.js

--- The old switcheroo 2.js
+++ The old switcheroo 2.js
@@ -15,3 +15,18 @@ String are case sensitive.
 // Bonus point if you don't use toLowerCase()
 */
 
+function encode(str) {
+  return str
+    .split("")
+    .map(v => (/[a-z]/gi.test(v) ? v.toLowerCase().charCodeAt() - 96 : v))
+    .join("");
+}
+
+/*
+const encode = str => {
+  return str.replace(/[a-z]/gi, l => {
+    let cc = l.charCodeAt(0);
+    return cc - (cc < 97 ? 64 : 96);
+  });
+}
+*/
