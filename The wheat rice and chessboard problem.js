7db42534a3b6f5eebcf33774ed917afea45f3d86
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-08 23:48:06 -0300

    Update and rename The wheat/rice and chessboard problem.js to The wheat rice and chessboard problem.js

--- The wheat rice and chessboard problem.js
+++ The wheat rice and chessboard problem.js
@@ -15,3 +15,14 @@ and etc.
 Input is always going to be valid/reasonable: ie: a non negative number; extra cookie for not using a loop to compute square-by-square (at least not directly) and instead trying a smarter approach [hint: some peculiar operator]; a trick converting the number might also work: impress me!
 */
 
+function squaresNeeded(grains){
+  const arr = [0];
+  for (let i = 0; i < 63; i++) {
+    arr.push(Math.pow(2, i));
+  }
+  for (let i = 0; i < 63; i++) {
+    if (grains < arr[i]) {
+      return i - 1;
+    }
+  }
+}
