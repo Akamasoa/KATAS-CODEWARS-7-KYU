7c7c62a94eb714de8262727eddfebc0dc3ad59db
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-11 13:56:26 -0300

    Update Thinkful - Object Drills: Quarks.js

--- Thinkful - Object Drills: Quarks.js
+++ Thinkful - Object Drills: Quarks.js
@@ -29,3 +29,16 @@ Example
 "red"
 */
 
+class Quark {
+  constructor(color, flavor) {
+    this.color = color;
+    this.flavor = flavor;
+    this.baryon_number = 1 / 3;
+  }
+  interact(obj) {
+    const color1 = obj.color;
+    const color2 = this.color;
+    this.color = color1;
+    obj.color = color2;
+  }
+}
