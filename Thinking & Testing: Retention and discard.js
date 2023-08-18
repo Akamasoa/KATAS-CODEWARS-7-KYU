4999082378d496d8d72371f63085aa982dc15bed
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-17 23:09:55 -0300

    Update Thinking & Testing: Retention and discard.js

--- Thinking & Testing: Retention and discard.js
+++ Thinking & Testing: Retention and discard.js
@@ -51,3 +51,8 @@ Play Tetris : Shape anastomosis
 Play FlappyBird : Advance Bravely
 */
 
+function testit(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i += 2) if (n % i === 0) arr.push(i);
+  return arr;
+}
