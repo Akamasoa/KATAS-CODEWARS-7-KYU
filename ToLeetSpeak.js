1d7a529e4f85319406908f24bb7d252bb516ce93
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-20 16:46:10 -0300

    Update ToLeetSpeak.js

--- ToLeetSpeak.js
+++ ToLeetSpeak.js
@@ -41,3 +41,62 @@ In this kata we use a simple LeetSpeak dialect. Use this alphabet:
 }
 */
 
+const toLeetSpeak = s => s.replace(/[ABCEGHILOSTZ]/g, c => D[c]);
+
+const D = {
+  A: "@",
+  B: "8",
+  C: "(",
+  E: "3",
+  G: "6",
+  H: "#",
+  I: "!",
+  L: "1",
+  O: "0",
+  S: "$",
+  T: "7",
+  Z: "2"
+};
+
+/*
+function toLeetSpeak(str) {
+  let leetDictionary = {
+    A : '@',
+    B : '8',
+    C : '(',
+    D : 'D',
+    E : '3',
+    F : 'F',
+    G : '6',
+    H : '#',
+    I : '!',
+    J : 'J',
+    K : 'K',
+    L : '1',
+    M : 'M',
+    N : 'N',
+    O : '0',
+    P : 'P',
+    Q : 'Q',
+    R : 'R',
+    S : '$',
+    T : '7',
+    U : 'U',
+    V : 'V',
+    W : 'W',
+    X : 'X',
+    Y : 'Y',
+    Z : '2'
+  }
+  
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == ' ') {
+      result += ' ';
+    }
+    else result += leetDictionary[str[i]];
+  }
+  return result;
+}
+*/
