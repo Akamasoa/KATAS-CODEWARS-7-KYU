4c3c6cde958c90c812762a6479fe5cdf0e655883
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-20 16:38:27 -0300

    Update Time Converter: hours, minutes, seconds and milliseconds.js

--- Time Converter: hours, minutes, seconds and milliseconds.js
+++ Time Converter: hours, minutes, seconds and milliseconds.js
@@ -11,3 +11,9 @@ new Date(2016, 2, 8, 16, 42, 59)
 "16:42:59,000"
 */
 
+function convert(time) {
+  let ms = time.getMilliseconds();
+  if (ms.toString().length < 2) ms = "00" + ms;
+  if (ms.toString().length < 3) ms = "0" + ms;
+  return time.toTimeString().split(" ")[0] + `,${ms}`;
+}
