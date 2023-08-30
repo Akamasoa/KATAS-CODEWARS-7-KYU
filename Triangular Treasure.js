bf096f9ae60928bc6970744375d3cefcb3d68cf2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-08-29 23:07:52 -0300

    Update Triangular Treasure.js

--- Triangular Treasure.js
+++ Triangular Treasure.js
@@ -16,3 +16,11 @@ For example: (Input --> Output)
 -10 --> 0
 */
 
+// Return the nth triangular number
+function triangular( n ) {
+    let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i * 1;
+  }
+  return sum;
+}
