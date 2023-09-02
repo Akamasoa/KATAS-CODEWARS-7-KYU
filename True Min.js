fcd966c815e21386dd46cccfd47f8be5480386b9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-01 21:58:52 -0300

    Update True Min.js

--- True Min.js
+++ True Min.js
@@ -9,3 +9,12 @@ function min(a, b){
 }
 */
 
+function min(a, b) {
+  a === null && (a = 0);
+  b === null && (b = 0);
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
+  return a < b ? a : b;
+}
+
