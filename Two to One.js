acca0bd36f2671751956106de05e101abeebc73b
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-04 23:07:38 -0300

    Update Two to One.js

--- Two to One.js
+++ Two to One.js
@@ -14,5 +14,28 @@ longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("");
 
 /*
-
+function longest(s1, s2) {
+  // your code
+  let duplicates = {}, uniqueChars = [];
+  for(let i = 0; i < Math.max(s1.length,s2.length); i++){
+    if(i < s1.length){
+      if(duplicates[s1[i]]){
+        duplicates[s1[i]]++
+      } else {
+        duplicates[s1[i]] = 1;
+      }
+    }
+    if(i < s2.length){
+      if(duplicates[s2[i]]){
+        duplicates[s2[i]]++
+      } else {
+        duplicates[s2[i]] = 1;
+      }
+    }
+  }
+  for(let key in duplicates){
+    uniqueChars.push(key)
+  }
+  return uniqueChars.sort().join('')
+}
 */
