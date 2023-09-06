e93f26b52bba15b0c4a71d50418769d7967c6d8e
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-05 23:19:23 -0300

    Update Ultimate Array Reverser.js

--- Ultimate Array Reverser.js
+++ Ultimate Array Reverser.js
@@ -9,3 +9,14 @@ Output: {"!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"}
 Good luck!
 */
 
+const ultimateReverse = s => {
+  let str = s
+    .join("")
+    .split("")
+    .reverse();
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr.push(str.splice(0, s[i].length).join(""));
+  }
+  return arr;
+};
