b5b7d88edad1d85a7b2a25518920a3bd4d82867d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-15 20:27:28 -0300

    Update Vasya and Drinks.js

--- Vasya and Drinks.js
+++ Vasya and Drinks.js
@@ -19,3 +19,7 @@ orangeFraction("50 50 100") // =>  Output cocktail contains 66.6667 % of orange 
 orangeFraction("0 25 50 75") // => Output cocktail contains 37.5000 % of orange juice
 */
 
+function orangeFraction(fractions) {
+  let arr = fractions.split` `.reduce((a, b) => a + b * 1, 0);
+  return (arr / fractions.split` `.length).toFixed(4) * 1;
+}
