efe11cd8a3bfcea3a1249a5851fc1939b91b797c
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-17 23:34:12 -0300

    Update Vowel Count.js

--- Vowel Count.js
+++ Vowel Count.js
@@ -7,3 +7,6 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 */
 
+function getCount(str) {
+  return (str.match(/[aeiou]/gi) || []).length;
+}
