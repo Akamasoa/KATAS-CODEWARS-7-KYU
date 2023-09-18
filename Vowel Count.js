9f2a93afe01734b77249efc7481740c200d24559
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-17 23:34:50 -0300

    Update Vowel Count.js

--- Vowel Count.js
+++ Vowel Count.js
@@ -10,3 +10,18 @@ The input string will only consist of lower case letters and/or spaces.
 function getCount(str) {
   return (str.match(/[aeiou]/gi) || []).length;
 }
+
+/*
+function getCount(str) {
+  let vowelsCount = 0;
+  let vowels = ["a","e","i","o","u"];
+  for(let i = 0;i < str.length;i++){
+    for(let j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  return vowelsCount;
+}
+*/
