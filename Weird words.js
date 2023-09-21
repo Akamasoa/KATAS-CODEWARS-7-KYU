63027318ed1e7aab959ed0037d66707f972b51b0
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-20 22:24:39 -0300

    Update Weird words.js

--- Weird words.js
+++ Weird words.js
@@ -13,3 +13,13 @@ Examples
 "zzZAaa"              -->  "aaABbb"
 */
 
+function nextLetter(str) {
+ return str.replace(/[a-zA-Z]/g, function(c){
+       switch (c) {
+          case 'z': return 'a';
+          case 'Z': return 'A';
+          default:  return String.fromCharCode(c.charCodeAt(0) +1);
+        } 
+    });
+}
+
