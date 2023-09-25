3b2f4f20fe2522d5debeadd3deb8b7f5567f368a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-24 22:36:07 -0300

    Update White or Black_.js

--- White or Black_.js
+++ White or Black_.js
@@ -10,3 +10,8 @@ Examples
 "f", 5  ==>  "white"
 */
 
+function mineColor(file, rank) {
+  const dict = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 };
+   return dict[file] % 2 === rank % 2 ? "black" : "white";
+}
+
