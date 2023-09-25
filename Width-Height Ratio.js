ee18425da495a7811ba2b1d34b4f409f58fb0b6a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-24 22:44:40 -0300

    Update Width-Height Ratio.js

--- Width-Height Ratio.js
+++ Width-Height Ratio.js
@@ -5,3 +5,24 @@ We all use 16:9, 16:10, 4:3 etc. ratios every day. Main task is to determine ima
 Function should take width and height of an image and return a ratio string (ex."16:9"). If any of width or height entry is 0 function should throw an exception (or return Nothing).
 */
 
+// Function should take width and height of an image and return a ratio string (ex."16:9").
+//If any of width or height entry is 0 function should raise an error.
+function calculateRatio(w, h){
+  if (w === 0 || h === 0) return "Error";
+  let gcd = (a, b) => (b ? gcd(b, a % b) : a);
+  const n = gcd(w, h);
+  return `${w / n}:${h / n}`;
+}
+
+/*
+function calculateRatio(w, h) {
+  if (!w || !h) throw new Error()
+  let r = gcd(w, h);
+  return (w / r) + ':' + (h / r)
+}
+
+function gcd(a, b) {
+  if (b === 0) return a
+  return gcd(b, a % b)
+}
+*/
