4ef28c5858c3ed44eb8c1d26bcbb0dd51ae4472d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-25 23:43:56 -0300

    Create We need these data immediately!.js

---
+++ We need these data immediately!.js
@@ -0,0 +1,12 @@
+/*
+Description:
+So we've had nifty-well-working javascript function
+
+returnSomeData()
+that was returning useful data, but recently it stopped work.
+
+There is some strange error. Can you find it out, what has changed?
+
+Tip: if in doubt, take a look at Mozilla's Lexical Grammar (thanks jpot for link!). Answer is somewhere here.
+*/
+
