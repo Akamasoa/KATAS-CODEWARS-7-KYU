742d2f9754f437114bae38b98a9f085a6457cea2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-09-27 21:30:36 -0300

    Update Words to sentence.js

--- Words to sentence.js
+++ Words to sentence.js
@@ -10,3 +10,15 @@ Example:
 function wordsToSentence(words) {
   return words.join` `;
 }
+
+/*
+const wordsToSentence = words => words.join(' ');
+
+function wordsToSentence(words) {
+  let x = "";
+  for(let i = 0; i < words.length; i++){
+    x = x + words[i] + " ";
+  }
+  return x.trim();
+}
+*/
