893c957ae245c55f4afe57f44b23522a8239db58
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-10-01 22:57:44 -0300

    Update #~For Kids~# d-m-Y -> Day of the week.js

--- #~For Kids~# d-m-Y -> Day of the week.js
+++ #~For Kids~# d-m-Y -> Day of the week.js
@@ -6,3 +6,17 @@ Example: "21/01/2017" -> "Saturday", "31/03/2017" -> "Friday"
 Have fun!
 */
 
+const moment = require("moment");
+function dayOfTheWeek(date) {
+  date = date.split("/");
+  let date1 = new Date(date[2], date[1] - 1, date[0]);
+  return [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ][moment(date1).day()];
+}
